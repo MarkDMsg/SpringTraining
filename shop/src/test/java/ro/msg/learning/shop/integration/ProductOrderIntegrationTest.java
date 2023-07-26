419210ca00f17866d33405d266a549b77aa539e3
MarkDMsg <Mark.Doszlop@msg.group>
2023-07-26 13:14:13 +0300

    Changed tests

--- shop/src/test/java/ro/msg/learning/shop/integration/ProductOrderIntegrationTest.java
+++ shop/src/test/java/ro/msg/learning/shop/integration/ProductOrderIntegrationTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -24,11 +23,6 @@ public class ProductOrderIntegrationTest {
 
     @Autowired
     private MockMvc mvc;
-
-    @BeforeAll
-    public void inializeTestData() {
-
-    }
 
     @Test
     public void testCreateOrderSuccessfully() throws Exception {
